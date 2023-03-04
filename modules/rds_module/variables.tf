04021fb0e5002c987b852e24b1dc4e0468eb6309
yusuf1207 <mukhammadturgunov@gmail.com>
2023-03-04 17:13:38 +0000

    main.tf setting.tf variables.tf added

--- modules/rds_module/variables.tf
+++ modules/rds_module/variables.tf
@@ -1,6 +1,11 @@
-variable "identifier" {
+variable "allocated_storage" {
+  type        = number
+  description = "The amount of storage to be allocated for the RDS instance"
+}
+
+variable "db_name" {
   type        = string
-  description = "The identifier for the RDS instance"
+  description = "The name of the database to be created when the RDS instance is created"
 }
 
 variable "engine" {
@@ -18,16 +23,6 @@ variable "instance_class" {
   description = "The instance class of the RDS instance"
 }
 
-variable "allocated_storage" {
-  type        = number
-  description = "The amount of storage to be allocated for the RDS instance"
-}
-
-variable "name" {
-  type        = string
-  description = "The name of the database to be created when the RDS instance is created"
-}
-
 variable "username" {
   type        = string
   description = "The username for the master user of the RDS instance"
@@ -43,12 +38,17 @@ variable "parameter_group_name" {
   description = "The name of the parameter group to be associated with the RDS instance"
 }
 
-variable "vpc_security_group_ids" {
-  type        = list(string)
-  description = "A list of security group IDs to associate with the RDS instance"
+variable "publicly_accessible" {
+  type = bool
+  description = "This varible represent publicly accessible added or not"
 }
 
 variable "skip_final_snapshot" {
   type        = bool
   description = "Specifies whether a final DB snapshot is created before the RDS instance is deleted"
+}
+
+variable "final_snapshot_identifier" {
+  type = bool
+  description = "This variable represents the final shot is added or not"
 }
